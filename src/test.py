271c12910a6529961911e34d9db8b7ca0bf9d9b5
mahmud624683 <mahmudul.hasan.bueteee@gmail.com>
2024-09-23 12:26:53 +0600

    daadad

---
+++ src/test.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import algo_methods
+
+folder_path = Path("libars_432")
+src_file = "bench_ckt/c432.bench"
+
+for file in folder_path.iterdir():
+    if file.is_file():
+        print(f"File: {file.name}")
+        print("SAT Attack: ", end="")
+        algo_methods.sat(src_file, "libars_432/"+file.name, max_iter=1000)
+        print("APPSAT Attack: ", end="")
+        algo_methods.appsat(src_file, "libars_432/"+file.name, max_iter=1000)
+        print("Sweep Attack: ", end="")
+        algo_methods.hamming_sweep(src_file, "libars_432/"+file.name, max_iter=1000)
+        print('\n')
+        
