cb2e3c41eda59f8206c34df7388d90992176531e
mahmud624683 <mahmudul.hasan.bueteee@gmail.com>
2024-09-26 14:17:36 +0600

    adasda

--- src/unlock_multicore.py
+++ src/unlock_multicore.py
@@ -6,28 +6,21 @@ import resource,signal
 
 folder_path = Path("libars_c1355/")
 src_file = "bench_ckt/c1355.bench"
-memory_limit_mb = 15  # Memory limit per process (in GB)
+memory_limit_gb = 4  # Memory limit per process (in GB)
 
 def limit_memory():
-    soft_limit = memory_limit_mb * 1024 * 1024 * 1024
+    soft_limit = memory_limit_gb * 1024 * 1024 * 1024
     resource.setrlimit(resource.RLIMIT_AS, (soft_limit, soft_limit))
 
-def memory_limit_exceeded(signum, frame):
-    raise MemoryError("Memory limit exceeded")
 
 def memory_limited_attack(file):
-    signal.signal(signal.SIGXCPU, memory_limit_exceeded)
     limit_memory()
 
     try:
-        if file.is_file():
-            print(file.name)
-            #algo_methods.sat(src_file, str(file), max_iter=1000, print_str = f"{file.name} SAT Attack: ", show_key = False)
-            #algo_methods.appsat(src_file, str(file), max_iter=1000, print_str = f"{file.name} APPSAT Attack: ", show_key = False)
-            algo_methods.hamming_sweep(src_file, str(file), max_iter=1000, print_str=f"{file.name} SWEEP Attack: ", show_key = False)
-    except MemoryError as e:
-        print(f"{file.name} Hamming Sweep : memory limit exceeded")
-        os._exit(1)
+        algo_methods.hamming_sweep(src_file, str(file), max_iter=1000, print_str=f"{file.name} SWEEP Attack: ", show_key=False)
+    except algo_methods.OutOfMemoryException:
+        print(f"{file.name} Hamming Sweep: C++ memory limit exceeded, continuing with next file")
+
     
 
 def process_file(file):
@@ -42,11 +35,11 @@ if __name__ == "__main__":
     files = [file.resolve() for file in folder_path.rglob('*') if file.is_file()]
 
     # Use all available CPU cores
-    num_workers = 2#cpu_count()
+    num_workers = cpu_count()
     with Pool(num_workers) as pool:
         pool.map(process_file, files)
 
-    """ # Use all available CPU cores
-    num_workers = 1
+    # Use all available CPU cores
+    """ num_workers = 1
     with Pool(num_workers) as pool:
         pool.map(memory_limited_attack, files) """
--- src/unlock_singlecore.py
+++ src/unlock_singlecore.py
@@ -5,26 +5,11 @@ import resource,signal
 folder_path = "libars_c1355"
 src_file = "bench_ckt/c1355.bench"
 
-memory_limit_mb = 7.5  # Memory limit per process (in GB)
-
-def limit_memory():
-    soft_limit = memory_limit_mb * 1024 * 1024 * 1024
-    resource.setrlimit(resource.RLIMIT_AS, (soft_limit, soft_limit))
-
-def memory_limit_exceeded(signum, frame):
-    raise MemoryError("Memory limit exceeded")
 
 for dirpath, _, filenames in os.walk(folder_path):
     for filename in filenames:
-        #signal.signal(signal.SIGXCPU, memory_limit_exceeded)
-        #limit_memory()
         # Construct absolute path of file
         lk_file = os.path.abspath(os.path.join(dirpath, filename))
-        print(f"File: {filename}")
-        print("SAT Attack: ", end="")
-        algo_methods.sat(src_file, lk_file, max_iter=1000)
-        print("APPSAT Attack: ", end="")
-        algo_methods.appsat(src_file, lk_file, max_iter=1000)
-        #print("Sweep Attack: ", end="")
-        #algo_methods.hamming_sweep(src_file, lk_file, max_iter=1000)
-        print('\n')
+        algo_methods.sat(src_file, lk_file, max_iter=1000, print_str = f"{filename} SAT Attack: ", show_key = False)
+        algo_methods.appsat(src_file, lk_file, max_iter=1000, print_str = f"{filename} APPSAT Attack: ", show_key = False)
+        #algo_methods.hamming_sweep(src_file, lk_file, max_iter=1000, print_str = f"{filename} SWEEP Attack: ", show_key = False)
