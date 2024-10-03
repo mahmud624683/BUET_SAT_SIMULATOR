a193d5ef5e5967b743955d8150838d345a164d78
mahmud624683 <mahmudul.hasan.bueteee@gmail.com>
2024-10-03 15:52:59 +0600

    dsdsdsd

--- src/unlock_multicore.py
+++ src/unlock_multicore.py
@@ -60,9 +60,9 @@ def memory_limit_exceeded(signum, frame):
     raise MemoryError("Memory limit exceeded\n")
 
 
-def process_file(file, time_limit = 1800, memory_limit = 0.5):
-    #signal.signal(signal.SIGXCPU, memory_limit_exceeded)
-    #limit_memory(memory_limit, file.name)
+def process_file(file, time_limit = 1800, memory_limit = 0.25):
+    signal.signal(signal.SIGXCPU, memory_limit_exceeded)
+    limit_memory(memory_limit, file.name)
 
     src_des = "bench_ckt"
     rslt = "src/raw_rslt.txt"
@@ -71,7 +71,7 @@ def process_file(file, time_limit = 1800, memory_limit = 0.5):
 
     if file.is_file():
         algo_name = []
-        #algo_name += ["SAT Attack", "APPSAT Attack"]#
+        algo_name += ["SAT Attack", "APPSAT Attack"]#
         algo_name += ["SWEEP Attack"]
         for algo in algo_name:
             start_time = datetime.now()
@@ -95,7 +95,7 @@ def main():
     files = [file.resolve() for file in folder_path.rglob('*') if file.is_file()]
 
     # Use all available CPU cores
-    num_workers = cpu_count()
+    num_workers = 4#cpu_count()
     with Pool(num_workers) as pool:
         pool.map(process_file, files)
         pool.close()
